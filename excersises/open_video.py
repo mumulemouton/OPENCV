0a1dab56a42a25a961df2419f4c15fae5710e8e9
Anita Tabaczka <atabaczka@edu.cdv.pl>
2021-12-27 00:19:31 +0100

    excersise

---
+++ excersises/open_video.py
@@ -0,0 +1,7 @@
+import cv2
+cap = cv2.VideoCapture("video/automatonism.mp4")
+while True:
+    success, img  = cap.read()
+    cv2.imshow("Video", img)
+    if cv2.cv2.waitKey(1) & 0xFF ==ord('q'):
+        break
